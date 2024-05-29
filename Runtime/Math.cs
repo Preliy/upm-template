f2a0d53d50338a92a14ed676b11c05958fe23755
Preliy <64234375+Preliy@users.noreply.github.com>
2024-05-30 00:09:19 +0200

    fix: Update Math.cs

--- Runtime/Math.cs
+++ Runtime/Math.cs
@@ -1,12 +1,18 @@
-public static class Math 
-{
-    public static float Add(float a, float b)
+namespace Preliy.upm-template.Runtime
+{ 
+    /// <summary>
+    /// A placeholder script that has some dummy data to show how documentation is generated
+    /// </summary>
+    public static class Math 
     {
-        return a + b;
-    }
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
     
-    public static float Minus(float a, float b)
-    {
-        return a - b; //feature 2
+        public static float Minus(float a, float b)
+        {
+            return a - b; //feature 2
+        }
     }
 }
